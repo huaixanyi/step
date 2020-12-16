2afb3476ccc060e37be5c99b6ae52bf2fe4c7a94
yancheng <lechebangyc>
2020-12-16 13:57:43 +0800

    first commit

---
+++ src/main/java/com/huaxianyi/step/learnstep/LearnStepApplication.java
@@ -0,0 +1,13 @@
+package com.huaxianyi.step.learnstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnStepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnStepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huaxianyi/step/learnstep/LearnStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huaxianyi.step.learnstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnStepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
